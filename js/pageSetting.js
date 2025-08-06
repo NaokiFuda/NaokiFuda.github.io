dd2f7a5eb2b1952f720395905dec732840e9e9a8
gizmo-pants <gigis.coutings.quad@gmail.com>
2025-08-06 23:29:51 +0900

    画像の位置の調節

--- js/pageSetting.js
+++ js/pageSetting.js
@@ -45,9 +45,9 @@ window.onload = function () {
     popup.classList.add('active');
     const popcontents = popup.querySelector(".popupContent");
     if(!popup.querySelector("noscroll")){
-      const height = popcontents.querySelector("img").clientHeight;
-      updateTransform(popcontents,0,height/2,1);
-      offsetX =0;offsetY = 0;
+      const height = popcontents.querySelector("img").clientHeight / 1.8;
+      updateTransform(popcontents,0,height,1);
+      offsetX =0;offsetY = height;
     }
     else{
 
